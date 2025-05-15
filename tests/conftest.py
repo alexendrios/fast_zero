ab567c9a316116b4bd3767b2cb354921ec9e80a2
Alexandre Pereira Santos <alex.endrios@hotmail.com>
2025-05-15 20:48:23 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,24 +6,32 @@ from fastapi.testclient import TestClient
 from markupsafe import Markup
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from fast_zero.app import app
-from fast_zero.models import table_registry
+from fast_zero.database import get_session
+from fast_zero.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
 
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
 
-@pytest.fixture
-def mock_db_time():
-    return _mock_db_time
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
@@ -34,17 +42,35 @@ def session():
 
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
-    def fake_time_hook(mapper, connection, target):
+    def fake_time_handler(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
 
-    event.listen(model, 'before_insert', fake_time_hook)
+    event.listen(model, 'before_insert', fake_time_handler)
 
     yield time
 
-    event.remove(model, 'before_insert', fake_time_hook)
+    event.remove(model, 'before_insert', fake_time_handler)
 
 
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testtest')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
+
+
+# Relatório HTML do Pytest (se estiver usando pytest-html)
 @pytest.hookimpl(tryfirst=True)
 def pytest_html_report_title(report):
     report.title = 'Relatório de Testes Unitários'
